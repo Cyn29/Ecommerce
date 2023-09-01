16cee0f176bd768a33c55fb0e82d6dedefa81242
raul-ra <raul.radillo@gmail.com>
2023-09-01 19:32:57 +0200

    Header testing with Bootstrap in development

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,38 @@
-import "./Header.css";
-import logo from "../../assets/logo.jpg";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Image from 'react-bootstrap/Image';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Header() {
-    return (
-        <>
-            <div className="header">
-                <nav className="navbar">
-                    <div className="navbar__logo">
-                        <img src={logo} />
-                    </div>
-                    <div className="navbar__items">
-                        <ul className="navbar__links">
-                            <li>HOME</li>
-                            <li>PRODUCTS</li>
-                            <li>CONTACT</li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </>
-    );
+  return (
+    <>
+        <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Row className="w-100 align-items-center justify-content-between">
+            <Col md={2} className="me-auto">
+              <Image src="../../assets/logo.jpg"/>
+            </Col>
+            <Col md={9} className=" w-50 justify-content-end ms-auto">
+              <Nav className="navbar">
+                <Col md={3}>
+                <Nav.Link href="#home">Home</Nav.Link>
+                </Col>
+                <Col md={3}>
+                <Nav.Link href="#features">Features</Nav.Link>
+                </Col>
+                <Col md={3}>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Col>
+              </Nav>
+            </Col>
+          </Row>
+        </Container>
+        </Navbar>
+    </>
+  );
 }
 
 export default Header;
