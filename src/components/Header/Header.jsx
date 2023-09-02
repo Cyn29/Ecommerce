49e53655fa606782be7e19af5a5e2f23a60d1b3e
Cynthia <cyn2993@gmail.com>
2023-09-02 23:08:32 +0200

    Add navbar with react-bootstrap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,40 @@
-import "./Header.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Image } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "../../assets/logo.jpg";
-import logout from "../../assets/icons/logout.png"
+import logouticon from "../../assets/icons/logout.png";
 
 function Header() {
     return (
-        <>
-            <div className="header">
-                <nav className="navbar">
-                    <div >
-                        <img className="navbar__logo" src={logo} />
-                    </div>
-                    <div className="navbar__items">
-                        <ul className="navbar__links">
-                            <li>PRODUCTS</li>
-                            <li>CONTACT</li>
-                            <li><img className="navbar__logout" src={logout}/></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </>
+        <Navbar expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <Image
+                        width="25%"
+                        height="20%"
+                        src={logo}
+                        alt="Image of logotype"
+                        fluid
+                    />
+                </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse  id="basic-navbar-nav">
+                        <Nav className="justify-content-end ml-auto">
+                            <Nav.Link href="#products">Products</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                            <Image
+                                src={logouticon}
+                                width="5%"
+                                height="1%"
+                                alt="Logout icon"
+                                fluid
+                            />
+                        </Nav>
+                    </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
