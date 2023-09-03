8dbb5ab69a96298ee407676e79b653dfa28cf832
Jaime Martínez <jimi_jr_9@hotmail.com>
2023-09-03 11:32:14 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 
 import './App.css'
-
+import Footer from './components/Footer/Footer';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 function App() {
 
   return (
     <>
+    <Footer />
     </>
   )
 }
