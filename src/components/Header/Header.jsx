7b79bc0783dc00fabde866551dc3560f7374a74a
Jaime Martínez <jimi_jr_9@hotmail.com>
2023-09-04 09:29:03 +0200

    change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Image } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "../../assets/logo.jpg";
 import logouticon from "../../assets/icons/logout.png";
 
