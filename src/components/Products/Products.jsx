9583d25283094926d2634c2a38dab4e0c81c4ca7
Jaime Martínez <jimi_jr_9@hotmail.com>
2023-09-04 12:52:22 +0200

    Add background in cards section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,27 +1,26 @@
-import { useContext } from "react"
-import { dataContext } from "../Context/DataContext"
+import { useContext } from "react";
+import { dataContext } from "../Context/DataContext";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 const Products = () => {
-    const { data } = useContext(dataContext)
-    return data.map((product) => {
+    const { data } = useContext(dataContext);
 
-
-        return (
-
-
-            <Card style={{ width: '18rem' }} className="mx-auto mt-4 mb-4 " key={product.id}>
-                <Card.Img variant="top" src={product.img} />
-                <Card.Body>
-                    <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>{product.description}</Card.Text>
-                    <Card.Text>{product.price}</Card.Text>
-                    <Button variant="primary" className="bg-danger">Ver Producto</Button>
-                </Card.Body>
-            </Card>
-        );
-    }
-    )
+    return (
+        <section className="p-3 mb-2 bg-dark text-white">
+            {data.map((product) => (
+                <Card style={{ width: '18rem' }} className="mx-auto mt-4 mb-4" key={product.id}>
+                    <Card.Img variant="top" src={product.img} />
+                    <Card.Body>
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>{product.description}</Card.Text>
+                        <Card.Text>{product.price}</Card.Text>
+                        <Button variant="primary" className="bg-danger">Ver Producto</Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </section>
+    );
 };
-export default Products
+
+export default Products;
