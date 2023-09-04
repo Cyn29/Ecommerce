1eff03bc4901c82a819e6c5f628aae00b07b7bae
raul-ra <raul.radillo@gmail.com>
2023-09-04 21:49:58 +0200

    npm install prop-types;add- import PropTypes form 'prop-Types-into-app.jsx

--- src/components/Context/DataContext.jsx
+++ src/components/Context/DataContext.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { createContext, useState, useEffect } from "react";
+import PropTypes from 'prop-types';
+
 
 export const dataContext = createContext();
 
@@ -9,7 +11,7 @@ const DataProvider = ({ children }) => {
     useEffect(() => {
         axios("http://localhost:3000/products")
             .then((res) => {
-               
+
                 setData(res.data);
                 console.log(res.data);
 
@@ -17,11 +19,14 @@ const DataProvider = ({ children }) => {
     }, []);
 
     return (
-        <dataContext.Provider value={{ data }}>
-            {children}
-        </dataContext.Provider>
-    );
-};
+            <dataContext.Provider value={{ data }}>
+                {children}
+            </dataContext.Provider>
+        );
+    };
+    DataProvider.propTypes = {
+        children: PropTypes.node.isRequired,
+    };
 
 export default DataProvider;
 
