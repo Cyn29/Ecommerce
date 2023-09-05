d8e56334c56c9547f580c3ed5c82bb995a1dbb9b
raul-ra <raul.radillo@gmail.com>
2023-09-05 20:33:47 +0200

    Updating buttons card product details

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Card, Container, Row, Col, Button } from 'react-bootstrap';
-import './ProductDetail.css'
+import EditIcon from '../../assets/icons/edit.png';
+import DeleteIcon from '../../assets/icons/delete.png';
+import UploadIcon from '../../assets/Uploadicon.png';
 
 function ProductDetail() {
   const [product, setProduct] = useState(null);
@@ -19,38 +21,48 @@ function ProductDetail() {
   }, []);
 
   if (!product) {
-    return <div id="ring" >Loading...</div>;
+    return <div id="ring">Loading...</div>;
   }
 
   return (
     <Container fluid className="bg-black">
-      <Card className="justify-content-md-auto">
-      <Row>
-        <Col  sm={6} className="d-flex justify-content-center">
-          {/* Product Image */}
-          <img src={product.img} alt="Product" className="w-100 h-100 img-fluid mx-auto"/>
-        </Col>
-        <Col sm={6}>
-          {/* Product Title */}
-          <h2 className="text-center" >{product.title}</h2>
-          {/* Product Description */}
-          <p className="text-center" >{product.fulldescription}</p>
-        </Col>
-      </Row>
-      <Row >
-      <Col sm={6}>
-          {/* Product price */}
-          <h3 className="text-center" >Price: €{product.price}</h3>
-        </Col>
-        <Col sm={3} className="d-flex justify-content-center align-items-center">
-          {/* Edit button */}
-          <Button variant="orange" className="text-dark">Edit</Button>
-        </Col>
-        <Col sm={3} className="d-flex justify-content-center align-items-center">
-          {/* Delete button */}
-          <Button variant="orange" className="text-dark">Delete</Button>
-        </Col>
-      </Row>
+      <Card>
+        <Row>
+          <Col sm={6} className="d-flex justify-content-center">
+            {/* Product Image */}
+            <img src={product.img} alt="Product" className="w-100 h-100 img-fluid mx-auto" />
+          </Col>
+          <Col sm={6}>
+            {/* Product Title */}
+            <h2 className="text-center">{product.title}</h2>
+            {/* Product Description */}
+            <p className="text-center">{product.fulldescription}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={6}>
+            {/* Product price */}
+            <h3 className="text-center">Price: €{product.price}</h3>
+          </Col>
+          <Col sm={2} className="d-flex justify-content-center align-items-center">
+            {/* Upload button */}
+            <Button variant="orange" className="w-100 h-100 p-0 mb-3" style={{ margin: '3px' }}>
+              <img src={UploadIcon} alt="upload" style={{ width: '1.5rem' }} />
+            </Button>
+          </Col>
+          <Col sm={2} className="d-flex justify-content-center align-items-center">
+            {/* Edit button */}
+            <Button variant="orange" className="w-100 h-100 p-0 mb-3" style={{ margin: '3px' }}>
+              <img src={EditIcon} alt="edit" style={{ width: '1.5rem' }} />
+            </Button>
+          </Col>
+          <Col sm={2} className="d-flex justify-content-center align-items-center">
+            {/* Delete button */}
+            <Button variant="orange" className="w-100 h-100 p-0 mb-3" style={{ margin: '3px' }}>
+              <img src={DeleteIcon} alt="delete" style={{ width: '1.5rem' }} />
+            </Button>
+          </Col>
+        </Row>
       </Card>
     </Container>
   );
