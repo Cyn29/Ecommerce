b02e6e8203830ef0d375d9322163c92fec2e4a21
raul-ra <raul.radillo@gmail.com>
2023-09-07 19:02:55 +0200

    Changing icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,37 @@
-
-import Header from './components/Header/Header';
-import Products from './components/Products/Products'
-import Contact from './components/Contact/Contact'
-import DataProvider from './components/Context/DataContext';
-import AddProductModal from './components/AddProductModal/AddProductModal';
-import ProductDetail from '../src/components/ProductDetail/ProductDetail';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom/dist';
-
-
+import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
+import Contact from "./components/Contact/Contact";
+import DataProvider from "./components/Context/DataContext";
+import AddProductModal from "./components/AddProductModal/AddProductModal";
+import ProductDetail from "../src/components/ProductDetail/ProductDetail";
+import { RouterProvider, createBrowserRouter } from "react-router-dom/dist";
 
 const router = createBrowserRouter([
-{
-path: '/',
-element: (
-<>
-<Header />
-<AddProductModal />
-<Products />
-<Footer />
-</>
-),
-},
-
-{
-path: '/productdetail/:productId',
-element: (
-<>
-<ProductDetail />
-</>
-),
-},
-
+  {
+    path: "/",
+    element: (
+      <>
+      <Header />
+      <AddProductModal />
+      <Products />
+      <Contact />
+      </>
+    ),
+  },
+  {
+    path: "/productdetail/:productId",
+    element: (
+      <>
+        <ProductDetail />
+      </>
+    ),
+  },
 ]);
-
-
 function App() {
-
   return (
-    
     <DataProvider>
-    <Header />
-    <AddProductModal />
-    <Products />
-    <Contact />
+      <RouterProvider router={router} />
     </DataProvider>
-      )
+  );
 }
-
-
 export default App;
-
