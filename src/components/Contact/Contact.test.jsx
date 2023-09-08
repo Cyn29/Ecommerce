71994e038cdd15b18714775578a9bdbd07c76a29
Cynthia <cyn2993@gmail.com>
2023-09-08 08:51:55 +0200

    Delete commented text

--- src/components/Contact/Contact.test.jsx
+++ src/components/Contact/Contact.test.jsx
@@ -1,22 +1,22 @@
 /* eslint-disable no-undef */
 
-import Contact from './Contact'
-import {render, screen} from '@testing-library/react'
+import Contact from "./Contact";
+import { render, screen } from "@testing-library/react";
 
-test('renders copyright', () => {
-    render(<Contact />)
-    const copyright = screen.getByText("© 2023 Copyright:")
-    expect(copyright).toBeDefined()
-})
+test("renders copyright", () => {
+    render(<Contact />);
+    const copyright = screen.getByText("© 2023 Copyright:");
+    expect(copyright).toBeDefined();
+});
 
-test('renders web', () => {
-    render(<Contact />)
-    const webUrl = screen.getByText("www.atuspies.com")
-    expect(webUrl).toBeDefined()
-})
+test("renders web", () => {
+    render(<Contact />);
+    const webUrl = screen.getByText("www.atuspies.com");
+    expect(webUrl).toBeDefined();
+});
 
-test('renders footer icons', () => {
-    render(<Contact />)
-    const footerIcons = screen.getAllByRole('button')
-    expect(footerIcons).toBeDefined()
-})
+test("renders footer icons", () => {
+    render(<Contact />);
+    const footerIcons = screen.getAllByRole("button");
+    expect(footerIcons).toBeDefined();
+});
--- src/components/Products/Products.test.jsx
+++ src/components/Products/Products.test.jsx
@@ -3,31 +3,6 @@
 import Products from "./Products";
 import { render, screen } from "@testing-library/react";
 
-
-/*vi.mock("../Context/DataContext.jsx", async () => {
-    const actual = await vi.importActual("../Context/DataContext.jsx")
-    return {
-      ...actual,
-      
-    },
-  })*/
-
-/*vi.mock("../Context/DataContext.jsx", () => ({
-    
-    useContext: vi.fn(() => ({
-        DataContext: vi.fn(()=>( {
-            name: "NIKE AIR MAX 1",
-            description:
-                "Con una pisada fluida para cualquier carrera, el modelo de Pegasus perfecto para ti vuelve para ayudarte a alcanzar tus objetivos.",
-            fulldescription:
-                "Con una pisada fluida para cualquier carrera, el modelo de Pegasus perfecto para ti vuelve para ayudarte a alcanzar tus objetivos. Esta versión ofrece la misma reactividad y sujeción neutra que tanto te gusta, pero con más comodidad en las zonas sensibles del pie, como el puente y los dedos. Da igual si estás en una maratón, haciendo sesiones de velocidad antes del amanecer o corriendo cuando te apetece",
-            price: "100",
-            img: "../src/assets/images/nike/nike6.jpg",
-            id: 1,
-        })),
-})),
-}));*/
-
 test("renders product image", async () => {
     render(<Products />);
     const image = await screen.findAllByRole("img");
